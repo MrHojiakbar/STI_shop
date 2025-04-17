32e69b07ab39b60339792235acb577e4ab0c182c
Hojiakbar <hojiakbar.offical28@gmail.com>
2025-04-18 00:17:51 +0500

    client strukture created

---
+++ client/src/main.js
@@ -0,0 +1,8 @@
+import "bootstap/dist/css/bootstrap.min.css";
+import "bootstap/dist/js/bootstrap.bundle.min.js";
+
+// import customAxios from "./"
+
+if (module.hot) {
+    module.hot.accept();
+}
