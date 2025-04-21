8d0d2e8bbaca61a93203833e57adb26381fcc130
Hojiakbar <hojiakbar.offical28@gmail.com>
2025-04-21 14:39:26 +0500

    frontend updated

--- client/src/js/register.js
+++ client/src/js/register.js
@@ -1,5 +1,7 @@
+import customAxios from "../configs/axios.config.js";
 const togglePassword = document.querySelector("#togglePassword");
 const password = document.querySelector("#password");
+const formEL = document.querySelector("form");
 
 togglePassword.addEventListener("click", function () {
   const type =
@@ -7,4 +9,21 @@ togglePassword.addEventListener("click", function () {
   password.setAttribute("type", type);
   this.classList.toggle("fa-eye");
   this.classList.toggle("fa-eye-slash");
+});
+
+
+formEL.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(formEL);
+  const data = Object.fromEntries(formData.entries());
+
+  customAxios
+    .post("/user/register", data)
+    .then((response) => {
+      console.log(response.data);
+      window.location.href = "/";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
